5f27aa3009251d6fe13b0c32faf28c40d95a1813
Pratyush <pratyushbhardwaj95@gmail.com>
2026-02-03 15:36:23 +0530

    Updated resume file added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/latest_res.pdf"
+                href="/My_latest_res.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
